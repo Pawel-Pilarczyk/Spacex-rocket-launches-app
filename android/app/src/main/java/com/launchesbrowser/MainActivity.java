4a393d92e783408b464960a8aa2bc3901c082462
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2022-05-16 21:55:02 +0200

    Creating splash screen and app icon

--- android/app/src/main/java/com/launchesbrowser/MainActivity.java
+++ android/app/src/main/java/com/launchesbrowser/MainActivity.java
@@ -1,11 +1,14 @@
 package com.launchesbrowser;
+
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -15,6 +18,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
